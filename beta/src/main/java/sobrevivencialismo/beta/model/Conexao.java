43ba0547591c337b5bcc141222a9675d1e616907
Allan <allanf333@gmail.com>
2021-12-20 18:36:17 -0300

    segund commit

---
+++ beta/src/main/java/sobrevivencialismo/beta/model/Conexao.java
@@ -0,0 +1,10 @@
+package sobrevivencialismo.beta.model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Conexao {
+    public Connection abrirConecxao() throws SQLException {
+        
+    }
+}
